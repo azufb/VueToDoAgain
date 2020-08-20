87902e90f23c8646a257560d487a8fce0310e8d5
Azusa Okamoto <curiousazunas@gmail.com>
2020-08-20 13:53:50 +0900

    Storeのsを小文字に変更

--- todoappagain/src/Store.js
+++ todoappagain/src/Store.js
@@ -1,8 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-Vue.use(vuex);
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-    strict: true
+    strict: true,
+    state: {
+        tasks: [
+            {
+                title: 'お弁当を買う。'
+            }
+        ]
+    },
+    mutations: {
+        addTask(state, payload) {
+            state.tasks.push(payload.task);
+        }
+    }
 })
