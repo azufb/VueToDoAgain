1950b16f7900dc4244e4e742bd2c628ce2720ca0
Azusa Okamoto <curiousazunas@gmail.com>
2020-08-20 13:56:29 +0900

    Storeのsを小文字に変更

--- todoappagain/src/main.js
+++ todoappagain/src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Store from './Store'
+import store from './Store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  Store,
+  store,
   render: h => h(App),
 }).$mount('#app')
